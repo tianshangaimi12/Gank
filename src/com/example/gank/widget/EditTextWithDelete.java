d3b87ff2a8ce45e009d1d20b2afc2579ec04b765
zhangchong <731411677@qq.com>
2017-09-28 14:22:16 +0800

    使用自定义的带删除按钮的编辑栏

--- src/com/example/gank/widget/EditTextWithDelete.java
+++ src/com/example/gank/widget/EditTextWithDelete.java
@@ -1,12 +1,9 @@
 package com.example.gank.widget;
 
 import com.example.gank.main.R;
-import android.R.drawable;
 import android.content.Context;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.support.v7.internal.widget.ContentFrameLayout;
-import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.EditText;
@@ -16,60 +13,64 @@ public class EditTextWithDelete extends EditText{
 	private static final int DRAWABLE_TOP = 1;
 	private static final int DRAWABLE_RIGHT = 2;
 	private static final int DRAWABLE_BOTTOM = 3;
-	private Drawable mDrawable;
+	private Drawable mClearDrawable;
 
 	public EditTextWithDelete(Context context)
 	{
-		this(context, null);
+		super(context);
+		init();
 	}
 	
 	public EditTextWithDelete(Context context, AttributeSet attrs)
 	{
-		this(context, attrs, 0);
+		super(context, attrs);
+		init();
 	}
 	
 	public EditTextWithDelete(Context context, AttributeSet attrs,
 			int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		init();
+	}
+	
+	private void init() {
+		mClearDrawable = getResources().getDrawable(R.drawable.delete);
 	}
 	
 	@Override
 	protected void onTextChanged(CharSequence text, int start,
 			int lengthBefore, int lengthAfter) {
 		super.onTextChanged(text, start, lengthBefore, lengthAfter);
-		setDeleteDrawableVisible(hasFocus() && text.length()>0);
+		setClearIconVisible(hasFocus() && text.length() > 0);
 	}
-	
+
 	@Override
 	protected void onFocusChanged(boolean focused, int direction,
 			Rect previouslyFocusedRect) {
 		super.onFocusChanged(focused, direction, previouslyFocusedRect);
-		setDeleteDrawableVisible(focused && length()>0);
-		
+		setClearIconVisible(focused && length() > 0);
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_UP:
-			float x = event.getX();
 			Drawable drawable = getCompoundDrawables()[DRAWABLE_RIGHT];
-			if(drawable != null && x<=(getWidth() - getPaddingRight())
-					&& x>=(getWidth()-getPaddingRight()-drawable.getBounds().width()));
-			setText("");
-			break;
-
-		default:
+			if (drawable != null
+					&& event.getX() <= (getWidth() - getPaddingRight())
+					&& event.getX() >= (getWidth() - getPaddingRight() - drawable
+							.getBounds().width())) {
+				setText("");
+			}
 			break;
 		}
 		return super.onTouchEvent(event);
 	}
-	
-	public void setDeleteDrawableVisible(boolean visibility)
-	{
+
+	private void setClearIconVisible(boolean visible) {
 		setCompoundDrawablesWithIntrinsicBounds(
 				getCompoundDrawables()[DRAWABLE_LEFT],
-				getCompoundDrawables()[DRAWABLE_TOP], visibility ? mDrawable
+				getCompoundDrawables()[DRAWABLE_TOP], visible ? mClearDrawable
 						: null, getCompoundDrawables()[DRAWABLE_BOTTOM]);
 	}
 
